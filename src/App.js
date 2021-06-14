06d84d5cdc1a697a30de8f0db944c386edd79946
Guy Vider <guy@guyvider.com>
2021-06-14 09:33:22 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import Web3 from 'web3';
+import Chains from './chains.json';
 import './App.css';
 
 function App() {
+  const [account, setAccount] = useState(undefined);
+  const [network, setNetwork] = useState(undefined);
+  const [error, setError] = useState('');
+  const {ethereum} = window;
+  const web3 = ethereum ? new Web3(ethereum) : undefined;
+    
+  const getAccount = async () => {
+    const chainId = await web3.eth.getChainId();
+    setNetworkName(chainId);
+    const accounts = await web3.eth.getAccounts();
+    setAccount(accounts[0]);  //there can be more than one connected accounts!
+  };
+
+  const setNetworkName = chainId => {
+    chainId = parseInt(chainId, 16);
+    console.log(`connected to chainID ${chainId}`);
+    let chain = Chains.find(c => c.chainId === chainId);
+    setNetwork(chain ? chain.name : 'unknown');
+  };
+
+  useEffect(() => {
+    const {ethereum} = window;
+    //if there's a wallet
+    if(web3) {
+      getAccount();
+      ethereum
+        .on('accountsChanged', accounts => setAccount(accounts[0]))
+        .on('chainChanged', chainId => setNetworkName(chainId));
+    }
+    else {
+      console.error('no wallet installed!');
+      setError('Please install a web3 wallet (like Metamask) and refresh the page');
+    }
+  }, []);
+
+  const connect = async () => {
+    const accounts = await web3.eth.requestAccounts();
+    console.log(accounts);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {
+      error
+      ?
+        <div style={{color: 'red'}}>${error}</div>
+        :
+        account 
+        ?
+          <>
+            <div>Account: {account}</div>
+            <div>Network: {network}</div>
+          </>
+          : 
+          <button onClick={connect}>Connect</button>
+    }
     </div>
   );
 }
